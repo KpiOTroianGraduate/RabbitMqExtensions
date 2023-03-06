693016531b56adf56125198ec51f5ac487ad07ad
C@H9 <49860805+Hapan9@users.noreply.github.com>
2023-03-06 19:40:22 +0200

    Main version

---
+++ RabbitMQ.Client.RepeatQueues/QueueDeleteExtensions.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client.RepeatQueues.Models;
+
+namespace RabbitMQ.Client.RepeatQueues;
+
+public static class QueueDeleteExtensions
+{
+    public static void QueueDeleteWithRepeat(this IModel model, QueuesParams queriesParams)
+    {
+        model.ExchangeDelete(queriesParams.ExchangeQueueName);
+        model.QueueDelete(queriesParams.MainQueueName);
+        model.QueueDelete(queriesParams.RepeatedQueueName);
+    }
+}
