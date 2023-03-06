971cb0d9e5b438903426f41cda8fb4ecf79cfdfa
C@H9 <49860805+Hapan9@users.noreply.github.com>
2023-03-06 21:33:50 +0200

    Package publish setting

--- RabbitMQ.Client.RepeatQueues/Models/QueuesParams.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RabbitMQ.Client.RepeatQueues.Models;
-
-public class QueuesParams
-{
-    public string MainQueueName { get; set; } = null!;
-    public string RepeatedQueueName { get; set; } = null!;
-    public string ExchangeQueueName { get; set; } = null!;
-}
