231b7fb9bac1ae8b5a7067d0ec64f5752f056468
C@H9 <49860805+Hapan9@users.noreply.github.com>
2023-03-07 13:47:18 +0200

    Add unit tests
    100% coverage

--- RabbitMQ.Client.Extensions/ChannelConsumerAsyncExtensions.cs
+++ RabbitMQ.Client.Extensions/ChannelConsumerAsyncExtensions.cs
@@ -1,12 +1,11 @@
 ﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Extensions.Models;
 using RabbitMQ.Client.Extensions.RepeatQueues;
 
 namespace RabbitMQ.Client.Extensions;
 
 public static class ChannelConsumerAsyncExtensions
 {
-    public delegate Task ConsumerAction(BasicDeliverEventArgs ea);
-
     /// <summary>
     ///     Setup consumer to queue
     /// </summary>
@@ -15,7 +14,8 @@ public static class ChannelConsumerAsyncExtensions
     /// <param name="consumerAction">Action to do with message</param>
     /// <param name="useRequeue">Use requeue if action throws</param>
     /// <param name="repeatTime">Use delay in ms if throws</param>
-    public static void SetConsumerAsync(this IModel model, string queueName, ConsumerAction consumerAction,
+    public static AsyncEventingBasicConsumer SetConsumerAsync(this IModel model, string queueName,
+        ConsumerAction consumerAction,
         bool useRequeue = true, int repeatTime = 0)
     {
         var queueParams = model.SetupQueueNames(queueName);
@@ -40,5 +40,7 @@ public static class ChannelConsumerAsyncExtensions
                 model.BasicNack(@event.DeliveryTag, false, !useRepeatQueue && useRequeue);
             }
         };
+
+        return consumer;
     }
 }
