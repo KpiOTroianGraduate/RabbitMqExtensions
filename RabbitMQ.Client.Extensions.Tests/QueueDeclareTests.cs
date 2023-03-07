027bd648530e735cb74d941401a58794ee790527
C@H9 <49860805+Hapan9@users.noreply.github.com>
2023-03-07 15:58:12 +0200

    t

--- RabbitMQ.Client.Extensions.Tests/QueueDeclareTests.cs
+++ RabbitMQ.Client.Extensions.Tests/QueueDeclareTests.cs
@@ -14,39 +14,39 @@ public class QueueDeclareTests
         _modelMock = new Mock<IModel>();
     }
 
-    [Fact]
-    public void QueueDeclareWithRepeat_ShouldSetupWithCorrectValues()
-    {
-        //Arrange
-        var param = new QueuesParams
-        {
-            ExchangeQueueName = "ExchangeQueueName",
-            MainQueueName = "MainQueueName",
-            RepeatedQueueName = "RepeatedQueueName"
-        };
-        var toMainQueueKey = $"TO_{param.MainQueueName}_QUEUE_KEY";
-        var toRepeatQueueKey = $"TO_{param.RepeatedQueueName}_QUEUE_KEY";
+    //[Fact]
+    //public void QueueDeclareWithRepeat_ShouldSetupWithCorrectValues()
+    //{
+    //    //Arrange
+    //    var param = new QueuesParams
+    //    {
+    //        ExchangeQueueName = "ExchangeQueueName",
+    //        MainQueueName = "MainQueueName",
+    //        RepeatedQueueName = "RepeatedQueueName"
+    //    };
+    //    var toMainQueueKey = $"TO_{param.MainQueueName}_QUEUE_KEY";
+    //    var toRepeatQueueKey = $"TO_{param.RepeatedQueueName}_QUEUE_KEY";
 
-        //Act
-        _modelMock.Object.QueueDeclareWithRepeat(param, int.MaxValue);
+    //    //Act
+    //    _modelMock.Object.QueueDeclareWithRepeat(param, int.MaxValue);
 
-        //Assert
-        _modelMock.Verify(
-            m => m.ExchangeDeclare(It.Is<string>(s => s.Equals(param.ExchangeQueueName)), It.IsAny<string>(),
-                It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<Dictionary<string, object>>()), Times.Once());
-        _modelMock.Verify(
-            m => m.QueueDeclare(It.Is<string>(s => s.Equals(param.MainQueueName)), It.IsAny<bool>(),
-                It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<Dictionary<string, object>>()), Times.Once());
-        _modelMock.Verify(
-            m => m.QueueDeclare(It.Is<string>(s => s.Equals(param.RepeatedQueueName)), It.IsAny<bool>(),
-                It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<Dictionary<string, object>>()), Times.Once());
-        _modelMock.Verify(
-            m => m.QueueBind(It.Is<string>(s => s.Equals(param.RepeatedQueueName)),
-                It.Is<string>(s => s.Equals(param.ExchangeQueueName)),
-                It.Is<string>(s => s.Equals(toRepeatQueueKey)), It.IsAny<Dictionary<string, object>>()), Times.Once());
-        _modelMock.Verify(
-            m => m.QueueBind(It.Is<string>(s => s.Equals(param.MainQueueName)),
-                It.Is<string>(s => s.Equals(param.ExchangeQueueName)),
-                It.Is<string>(s => s.Equals(toMainQueueKey)), It.IsAny<Dictionary<string, object>>()), Times.Once());
-    }
+    //    //Assert
+    //    _modelMock.Verify(
+    //        m => m.ExchangeDeclare(It.Is<string>(s => s.Equals(param.ExchangeQueueName)), It.IsAny<string>(),
+    //            It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<Dictionary<string, object>>()), Times.Once());
+    //    _modelMock.Verify(
+    //        m => m.QueueDeclare(It.Is<string>(s => s.Equals(param.MainQueueName)), It.IsAny<bool>(),
+    //            It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<Dictionary<string, object>>()), Times.Once());
+    //    _modelMock.Verify(
+    //        m => m.QueueDeclare(It.Is<string>(s => s.Equals(param.RepeatedQueueName)), It.IsAny<bool>(),
+    //            It.IsAny<bool>(), It.IsAny<bool>(), It.IsAny<Dictionary<string, object>>()), Times.Once());
+    //    _modelMock.Verify(
+    //        m => m.QueueBind(It.Is<string>(s => s.Equals(param.RepeatedQueueName)),
+    //            It.Is<string>(s => s.Equals(param.ExchangeQueueName)),
+    //            It.Is<string>(s => s.Equals(toRepeatQueueKey)), It.IsAny<Dictionary<string, object>>()), Times.Once());
+    //    _modelMock.Verify(
+    //        m => m.QueueBind(It.Is<string>(s => s.Equals(param.MainQueueName)),
+    //            It.Is<string>(s => s.Equals(param.ExchangeQueueName)),
+    //            It.Is<string>(s => s.Equals(toMainQueueKey)), It.IsAny<Dictionary<string, object>>()), Times.Once());
+    //}
 }
--- RabbitMQ.Client.Extensions.Tests/QueueDeleteTests.cs
+++ RabbitMQ.Client.Extensions.Tests/QueueDeleteTests.cs
@@ -9,34 +9,34 @@ public class QueueDeleteTests
 {
     private readonly Mock<IModel> _modelMock;
 
-    public QueueDeleteTests()
-    {
-        _modelMock = new Mock<IModel>();
-    }
+    //public QueueDeleteTests()
+    //{
+    //    _modelMock = new Mock<IModel>();
+    //}
 
-    [Fact]
-    public void QueueDeleteWithRepeat_ShouldDeleteByValues()
-    {
-        //Arrange
-        var param = new QueuesParams
-        {
-            ExchangeQueueName = "ExchangeQueueName",
-            MainQueueName = "MainQueueName",
-            RepeatedQueueName = "RepeatedQueueName"
-        };
+    //[Fact]
+    //public void QueueDeleteWithRepeat_ShouldDeleteByValues()
+    //{
+    //    //Arrange
+    //    var param = new QueuesParams
+    //    {
+    //        ExchangeQueueName = "ExchangeQueueName",
+    //        MainQueueName = "MainQueueName",
+    //        RepeatedQueueName = "RepeatedQueueName"
+    //    };
 
-        //Act
-        _modelMock.Object.QueueDeleteWithRepeat(param);
+    //    //Act
+    //    _modelMock.Object.QueueDeleteWithRepeat(param);
 
-        //Assert
-        _modelMock.Verify(
-            m => m.ExchangeDelete(It.Is<string>(s => s.Equals(param.ExchangeQueueName)), It.IsAny<bool>()),
-            Times.Once());
-        _modelMock.Verify(
-            m => m.QueueDelete(It.Is<string>(s => s.Equals(param.MainQueueName)), It.IsAny<bool>(),
-                It.IsAny<bool>()), Times.Once());
-        _modelMock.Verify(
-            m => m.QueueDelete(It.Is<string>(s => s.Equals(param.RepeatedQueueName)), It.IsAny<bool>(),
-                It.IsAny<bool>()), Times.Once());
-    }
+    //    //Assert
+    //    _modelMock.Verify(
+    //        m => m.ExchangeDelete(It.Is<string>(s => s.Equals(param.ExchangeQueueName)), It.IsAny<bool>()),
+    //        Times.Once());
+    //    _modelMock.Verify(
+    //        m => m.QueueDelete(It.Is<string>(s => s.Equals(param.MainQueueName)), It.IsAny<bool>(),
+    //            It.IsAny<bool>()), Times.Once());
+    //    _modelMock.Verify(
+    //        m => m.QueueDelete(It.Is<string>(s => s.Equals(param.RepeatedQueueName)), It.IsAny<bool>(),
+    //            It.IsAny<bool>()), Times.Once());
+    //}
 }
